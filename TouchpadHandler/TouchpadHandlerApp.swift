cd5a78134fed648a6f058b527838c7bee01daba0
alexandr_sadunenko <a.sadunenko@gmail.com>
2023-05-17 13:08:03 +0300

    Initial Commit

---
+++ TouchpadHandler/TouchpadHandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchpadHandlerApp.swift
+//  TouchpadHandler
+//
+//  Created by Alex on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchpadHandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
